8795970c7618868debb7bdafc66d8ff4e063af9d
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-09-03 16:55:44 -0400

    commite function fist class

---
+++ funcoes_de_primeira_classe.js
@@ -0,0 +1,25 @@
+/*
+- Apartir do paradigma funcional nos podemos não somente colocar valores como string, boolean, int e etc em uma variável mais também colocar e tratar funções como um tipo
+
+- Então poderemos guardar valores de funções dentro de nossas variáveis.
+
+- Atenção!. como o paradigma funcional costuma trabalhar muito com o conceito de imutabilidade poderemos ver muito variáveis do tipo **const**
+*/
+
+const getNome = function () {
+    console.log("Jonas!.")
+    // retorna void
+}
+
+getNome();
+
+/*
+ ====== Reescrevendo código ========
+
+    const getNome = ()=> {
+	    console.log("Jonas!.")
+    }
+
+    getNome();
+
+*/
