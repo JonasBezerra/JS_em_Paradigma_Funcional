5a3ef22b50b320bbc4fe38a2ae97fff9a956d09f
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-09-07 14:05:59 -0400

    commite atualization
    atualização de comentarios no código e revisão

---
+++ 02_funcao_nao_bloqueante.js
@@ -0,0 +1,22 @@
+// Enquanto dois códigos ficam rodando ao mesmo tempo (em paralelo), nenhum interfere no outro
+
+setTimeout(function(){ 
+    console.log("Oi 1");
+}, 5000)
+
+const minhaFuncao = function(){
+console.log("Oi 3!")
+}
+
+setTimeout(minhaFuncao, 5000) 
+
+console.log("Oi 2!") // executa primeiro esse código
+
+// so depois então ele roda os dois códigos em paralelo.
+
+/*  
+so depois de 5000ms ela executará a função que tem a msg "Oi 1" e "Oi 3!."
+Ou seja para executar o console.log("Oi 2!") ela não depende que o
+método de cima seja executado. essa função não bloqueia a execução dos demais
+comandos isso se aplica nos dois que serão executados em paralelo
+*/
